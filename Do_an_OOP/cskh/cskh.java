a377c3213cc0bce2d5ccad6b91bd642303731e3b
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2023-11-11 23:31:35 +0700

    Add files via upload
    bổ sung thông tin trong cskh

---
+++ Do_an_OOP/cskh/cskh.java
@@ -0,0 +1,14 @@
+package com.cskh;
+public class cskh{
+    public static void main(String[] args) {
+                        System.out.println("Ban co the lien he voi cac thanh vien sau de duoc giai quyet voi toc do phan hoi tot nhat");
+                        System.out.println("Ten\t\t\tFacebook\t\tGmail\t\t\t\tSdt");
+                        System.out.println("Nguyen Dai Quoc\t\tfb.com/wolfdzai\t\tluutrithon1996@gmail.com\t0931454176");
+                        System.out.println("Dinh Phuc Thinh\t\tfb.com/dinfucthin\tzaikaman123@gmail.com\t\t0931816175");
+                        System.out.println("Tran Trung Viet\t\tcutt.ly/qwTNnwZY\t\t");
+                        System.out.println("Tran Dang Phat \t\tcutt.ly/TwTNb3Ur\t\t");
+                        System.out.println("Nguyen Hoang Sang\tcutt.ly/4wTNbGKf\tlesang01227982715@gmail.com\t0776592967");
+                        System.out.println("");
+                        System.out.println("");
+    }
+}
