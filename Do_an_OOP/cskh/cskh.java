399e98fad450b8812dee962be32abef452506aab
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-12 00:00:53 +0700

    Update cskh.java
    - Đã thêm các thuộc tính như sơ đồ

--- Do_an_OOP/cskh/cskh.java
+++ Do_an_OOP/cskh/cskh.java
@@ -1,14 +1,32 @@
 package com.cskh;
-public class cskh{
+
+public class ThongTinLienLac {
+    String ten;
+    String email;
+    String soDienThoai;
+    String linkMangXaHoi;
+
+    public ThongTinLienLac(String ten, String email, String soDienThoai, String linkMangXaHoi) {
+        this.ten = ten;
+        this.email = email;
+        this.soDienThoai = soDienThoai;
+        this.linkMangXaHoi = linkMangXaHoi;
+    }
+
     public static void main(String[] args) {
-                        System.out.println("Ban co the lien he voi cac thanh vien sau de duoc giai quyet voi toc do phan hoi tot nhat");
-                        System.out.println("Ten\t\t\tFacebook\t\tGmail\t\t\t\tSdt");
-                        System.out.println("Nguyen Dai Quoc\t\tfb.com/wolfdzai\t\tluutrithon1996@gmail.com\t0931454176");
-                        System.out.println("Dinh Phuc Thinh\t\tfb.com/dinfucthin\tzaikaman123@gmail.com\t\t0931816175");
-                        System.out.println("Tran Trung Viet\t\tcutt.ly/qwTNnwZY\t\t");
-                        System.out.println("Tran Dang Phat \t\tcutt.ly/TwTNb3Ur\t\t");
-                        System.out.println("Nguyen Hoang Sang\tcutt.ly/4wTNbGKf\tlesang01227982715@gmail.com\t0776592967");
-                        System.out.println("");
-                        System.out.println("");
+        ThongTinLienLac[] danhSach = new ThongTinLienLac[]{
+            new ThongTinLienLac("Nguyen Dai Quoc", "luutrithon1996@gmail.com", "0931454176", "fb.com/wolfdzai"),
+            new ThongTinLienLac("Dinh Phuc Thinh", "zaikaman123@gmail.com", "0931816175", "fb.com/dinfucthin"),
+            new ThongTinLienLac("Tran Trung Viet", "", "", "cutt.ly/qwTNnwZY"),
+            new ThongTinLienLac("Tran Dang Phat", "", "", "cutt.ly/TwTNb3Ur"),
+            new ThongTinLienLac("Nguyen Hoang Sang", "lesang01227982715@gmail.com", "0776592967", "cutt.ly/4wTNbGKf")
+        };
+
+        System.out.println("Ban co the lien he voi cac thanh vien sau de duoc giai quyet voi toc do phan hoi tot nhat");
+        System.out.println("Ten\t\t\tFacebook\t\tGmail\t\t\t\tSdt");
+
+        for (ThongTinLienLac lienLac : danhSach) {
+            System.out.println(lienLac.ten + "\t\t" + lienLac.linkMangXaHoi + "\t\t" + lienLac.email + "\t\t" + lienLac.soDienThoai);
+        }
     }
 }
