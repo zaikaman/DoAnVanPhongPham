735e9a29c1a4ababfff50c6b49ba350b71f88e45
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2023-11-11 23:32:11 +0700

    Add files via upload
    bổ sung thông tin

---
+++ Do_an_OOP/details/details.java
@@ -0,0 +1,27 @@
+package com.details;
+public class details{
+    public static void main(String[] args) {
+                        System.out.println("Chuong trinh Ban van phong pham");
+                        System.out.println("Version: 1.0.0");
+                        System.out.println("Release Date: November 2023");
+                        System.out.println("Dev team:");
+                        System.out.println("1. Nguyen Dai Quoc");
+                        System.out.println("   - Facebook: fb.com/wolfdzai");
+                        System.out.println("   - Email: luutrithon1996@gmail.com");
+                        System.out.println("   - Phone: 0931454176");
+                        System.out.println("2. Dinh Phuc Thinh");
+                        System.out.println("   - Facebook: fb.com/dinfucthin");
+                        System.out.println("   - Email: zaikaman123@gmail.com");
+                        System.out.println("   - Phone: 0931816175");
+                        System.out.println("3. Tran Trung Viet");
+                        System.out.println("   - Facebook: cutt.ly/qwTNnwZY");
+                        System.out.println("4. Tran Dang Phat");
+                        System.out.println("   - Facebook: cutt.ly/TwTNb3Ur");
+                        System.out.println("5. Nguyen Hoang Sang");
+                        System.out.println("   - Facebook: cutt.ly/4wTNbGKf");
+                        System.out.println("   - Email: lesang01227982715@gmail.com");
+                        System.out.println("   - Phone: 0776592967");
+                        System.out.println("");
+                        System.out.println("");
+    }
+}
