8ef90c02671bdcb93229d8511c4888594ca46440
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-12 10:32:48 +0700

    Update Main.java
    - Đã loại bỏ các API lỗi thời

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.Calendar;
 
 class DonHang {
     private String maDonHang;
@@ -521,36 +522,44 @@ class ThongTinChuongTrinh {
     }
 
     public static void displayProgramInfo() {
-        ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
-            "1.0.0",
-            "Chuong trinh Ban van phong pham",
-            "",
-            "1. Nguyen Dai Quoc\n" +
-            "   - Facebook: fb.com/wolfdzai\n" +
-            "   - Email: luutrithon1996@gmail.com\n" +
-            "   - Phone: 0931454176\n" +
-            "2. Dinh Phuc Thinh\n" +
-            "   - Facebook: fb.com/dinfucthin\n" +
-            "   - Email: zaikaman123@gmail.com\n" +
-            "   - Phone: 0931816175\n" +
-            "3. Tran Trung Viet\n" +
-            "   - Facebook: cutt.ly/qwTNnwZY\n" +
-            "4. Tran Dang Phat\n" +
-            "   - Facebook: cutt.ly/TwTNb3Ur\n" +
-            "5. Nguyen Hoang Sang\n" +
-            "   - Facebook: cutt.ly/4wTNbGKf\n" +
-            "   - Email: lesang01227982715@gmail.com\n" +
-            "   - Phone: 0776592967\n",
-            new Date(2023, 11, 8),
-            new Date(2023, 1, 1)
-        );
+    Calendar calStart = Calendar.getInstance();
+    calStart.set(2023, Calendar.NOVEMBER, 8);
+    Date startDate = calStart.getTime();
 
-        System.out.println("Chuong trinh: " + chuongTrinh.ten);
-        System.out.println("Version: " + chuongTrinh.maChuongTrinh);
-        System.out.println("Release Date: " + chuongTrinh.ngayBatDau);
-        System.out.println("Dev team:");
-        System.out.println(chuongTrinh.teamDev);
-    }
+    Calendar calEnd = Calendar.getInstance();
+    calEnd.set(2023, Calendar.JANUARY, 1);
+    Date endDate = calEnd.getTime();
+
+    ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
+        "1.0.0",
+        "Chuong trinh Ban van phong pham",
+        "",
+        "1. Nguyen Dai Quoc\n" +
+        "   - Facebook: fb.com/wolfdzai\n" +
+        "   - Email: luutrithon1996@gmail.com\n" +
+        "   - Phone: 0931454176\n" +
+        "2. Dinh Phuc Thinh\n" +
+        "   - Facebook: fb.com/dinfucthin\n" +
+        "   - Email: zaikaman123@gmail.com\n" +
+        "   - Phone: 0931816175\n" +
+        "3. Tran Trung Viet\n" +
+        "   - Facebook: cutt.ly/qwTNnwZY\n" +
+        "4. Tran Dang Phat\n" +
+        "   - Facebook: cutt.ly/TwTNb3Ur\n" +
+        "5. Nguyen Hoang Sang\n" +
+        "   - Facebook: cutt.ly/4wTNbGKf\n" +
+        "   - Email: lesang01227982715@gmail.com\n" +
+        "   - Phone: 0776592967\n",
+        startDate,
+        endDate
+    );
+
+    System.out.println("Chuong trinh: " + chuongTrinh.ten);
+    System.out.println("Version: " + chuongTrinh.maChuongTrinh);
+    System.out.println("Release Date: " + chuongTrinh.ngayBatDau);
+    System.out.println("Dev team:");
+    System.out.println(chuongTrinh.teamDev);
+}
 }
 
 
@@ -763,8 +772,15 @@ public class Main {
         new ThongTinLienLac("Nguyen Hoang Sang", "lesang01227982715@gmail.com", "0776592967", "cutt.ly/4wTNbGKf")
     };
 
-        // Declare ThongTinChuongTrinh related stuff
-        ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
+Calendar calStart = Calendar.getInstance();
+calStart.set(2023, Calendar.NOVEMBER, 8);
+Date startDate = calStart.getTime();
+
+Calendar calEnd = Calendar.getInstance();
+calEnd.set(2023, Calendar.JANUARY, 1);
+Date endDate = calEnd.getTime();
+
+ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
     "1.0.0",
     "Chuong trinh Ban van phong pham",
     "",
@@ -784,8 +800,8 @@ public class Main {
     "   - Facebook: cutt.ly/4wTNbGKf\n" +
     "   - Email: lesang01227982715@gmail.com\n" +
     "   - Phone: 0776592967\n",
-    new Date(2023, 11, 8),
-    new Date(2023, 1, 1)
+    startDate,
+    endDate
 );
 
 
