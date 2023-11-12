4029eb1a671ca1391f5be12db9c1bb1d48b0859c
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-12 11:41:57 +0700

    Update cskh.java

--- Do_an_OOP/cskh/cskh.java
+++ Do_an_OOP/cskh/cskh.java
@@ -1,5 +1,4 @@
-
-public class ThongTinLienLac {
+class ThongTinLienLac {
     String ten;
     String email;
     String soDienThoai;
@@ -12,15 +11,7 @@ public class ThongTinLienLac {
         this.linkMangXaHoi = linkMangXaHoi;
     }
 
-    public static void main(String[] args) {
-        ThongTinLienLac[] danhSach = new ThongTinLienLac[]{
-            new ThongTinLienLac("Nguyen Dai Quoc", "luutrithon1996@gmail.com", "0931454176", "fb.com/wolfdzai"),
-            new ThongTinLienLac("Dinh Phuc Thinh", "zaikaman123@gmail.com", "0931816175", "fb.com/dinfucthin"),
-            new ThongTinLienLac("Tran Trung Viet", "", "", "cutt.ly/qwTNnwZY"),
-            new ThongTinLienLac("Tran Dang Phat", "", "", "cutt.ly/TwTNb3Ur"),
-            new ThongTinLienLac("Nguyen Hoang Sang", "lesang01227982715@gmail.com", "0776592967", "cutt.ly/4wTNbGKf")
-        };
-
+    public static void displayContacts(ThongTinLienLac[] danhSach) {
         System.out.println("Ban co the lien he voi cac thanh vien sau de duoc giai quyet voi toc do phan hoi tot nhat");
         System.out.println("Ten\t\t\tFacebook\t\tGmail\t\t\t\tSdt");
 
