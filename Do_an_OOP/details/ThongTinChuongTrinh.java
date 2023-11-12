62babe11e7ac0348236fbfae5ba170b6f973ed9d
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-12 11:42:17 +0700

    Update ThongTinChuongTrinh.java

--- Do_an_OOP/details/ThongTinChuongTrinh.java
+++ Do_an_OOP/details/ThongTinChuongTrinh.java
@@ -1,4 +1,4 @@
-public class ThongTinChuongTrinh {
+class ThongTinChuongTrinh {
     String maChuongTrinh;
     String ten;
     String moTa;
@@ -15,35 +15,43 @@ public class ThongTinChuongTrinh {
         this.ngayKetThuc = ngayKetThuc;
     }
 
-    public static void main(String[] args) {
-        ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
-            "1.0.0",
-            "Chuong trinh Ban van phong pham",
-            "",
-            "1. Nguyen Dai Quoc\n" +
-            "   - Facebook: fb.com/wolfdzai\n" +
-            "   - Email: luutrithon1996@gmail.com\n" +
-            "   - Phone: 0931454176\n" +
-            "2. Dinh Phuc Thinh\n" +
-            "   - Facebook: fb.com/dinfucthin\n" +
-            "   - Email: zaikaman123@gmail.com\n" +
-            "   - Phone: 0931816175\n" +
-            "3. Tran Trung Viet\n" +
-            "   - Facebook: cutt.ly/qwTNnwZY\n" +
-            "4. Tran Dang Phat\n" +
-            "   - Facebook: cutt.ly/TwTNb3Ur\n" +
-            "5. Nguyen Hoang Sang\n" +
-            "   - Facebook: cutt.ly/4wTNbGKf\n" +
-            "   - Email: lesang01227982715@gmail.com\n" +
-            "   - Phone: 0776592967\n",
-            new Date(2023, 11, 8),
-            new Date(2023, 1, 1)
-        );
+    public static void displayProgramInfo() {
+    Calendar calStart = Calendar.getInstance();
+    calStart.set(2023, Calendar.NOVEMBER, 8);
+    Date startDate = calStart.getTime();
 
-        System.out.println("Chuong trinh: " + this.ten);
-        System.out.println("Version: " + this.maChuongTrinh);
-        System.out.println("Release Date: " + this.ngayBatDau);
-        System.out.println("Dev team:");
-        System.out.println(this.teamDev);
-    }
+    Calendar calEnd = Calendar.getInstance();
+    calEnd.set(2023, Calendar.JANUARY, 1);
+    Date endDate = calEnd.getTime();
+
+    ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
+        "1.0.0",
+        "Chuong trinh Ban van phong pham",
+        "",
+        "1. Nguyen Dai Quoc\n" +
+        "   - Facebook: fb.com/wolfdzai\n" +
+        "   - Email: luutrithon1996@gmail.com\n" +
+        "   - Phone: 0931454176\n" +
+        "2. Dinh Phuc Thinh\n" +
+        "   - Facebook: fb.com/dinfucthin\n" +
+        "   - Email: zaikaman123@gmail.com\n" +
+        "   - Phone: 0931816175\n" +
+        "3. Tran Trung Viet\n" +
+        "   - Facebook: cutt.ly/qwTNnwZY\n" +
+        "4. Tran Dang Phat\n" +
+        "   - Facebook: cutt.ly/TwTNb3Ur\n" +
+        "5. Nguyen Hoang Sang\n" +
+        "   - Facebook: cutt.ly/4wTNbGKf\n" +
+        "   - Email: lesang01227982715@gmail.com\n" +
+        "   - Phone: 0776592967\n",
+        startDate,
+        endDate
+    );
+
+    System.out.println("Chuong trinh: " + chuongTrinh.ten);
+    System.out.println("Version: " + chuongTrinh.maChuongTrinh);
+    System.out.println("Release Date: " + chuongTrinh.ngayBatDau);
+    System.out.println("Dev team:");
+    System.out.println(chuongTrinh.teamDev);
+}
 }
